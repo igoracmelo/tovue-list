547b9106c1899b7963f37d10917522758ef125f5
Igor Melo <igoracmelo@protonmail.com>
2021-08-31 02:29:18 -0300

    Fist commit

---
+++ src/components/TodoForm/TodoForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <form class="form" @submit.prevent="handleSubmit">
+    <input type="text" class="todo-text" v-model="todo.text">
+    <button type="submit" class="todo-add">Add</button>
+  </form>
+</template>
+
+
+
+<script>
+import App from '../App/App.vue'
+
+export default {
+  data() {
+    return {
+      todo: {},
+    }
+  },
+  
+  methods: {
+    handleSubmit() {
+      if (this.todo.text.match(/^\s*$/)) 
+          return
+      this.todo.id = parseInt(Math.random() * 10000000000)
+      console.log(this.todo)
+
+      this.todo.text = ""
+      this.todo.id = -1
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import './TodoForm.scss'
+</style>
---
+++ src/components/TodoItem/TodoItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="todo-item">
+    <input 
+      type="checkbox" 
+      class="todo-done focus-outline" 
+      v-model="done" 
+      @change="onCheck($event)"
+    >
+    <input type="text" class="todo-text" :value="todo">
+    <button 
+      class="todo-delete focus-outline"
+      @click="onDelete($event)">X</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: String,
+  },
+
+  data() {
+    return {
+      done: false
+    }
+  },
+
+  methods: {
+    onCheck(e) {
+      let currentTodo = e.target.parentNode
+      if (this.done)
+        currentTodo.classList.add("done")
+      else
+        currentTodo.classList.remove("done")
+    },
+
+    onDelete(e) {
+      let currentTodo = e.target.parentNode
+      currentTodo.classList.add("deleted")
+      setTimeout(() => currentTodo.remove(), 500)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import './TodoItem.scss';
+</style>
