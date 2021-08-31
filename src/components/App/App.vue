2b3e2f1792063be1d48347219a182d7eb9562920
Igor Melo <igoracmelo@protonmail.com>
2021-08-31 14:00:20 -0300

    Detached TodoContainer from App

--- src/components/App/App.vue
+++ src/components/App/App.vue
@@ -1,37 +1,15 @@
 <template>
-  <div class="todo-container">
-    <TodoForm />
-    <div class="todos">
-      <TodoItem 
-        v-for="item in todoItems" 
-        :key="item.id" 
-        :todo="item.text"
-      />
-    </div>
-  </div>
+  <TodoContainer />
 </template>
 
 <script>
-import TodoItem from '../TodoItem/TodoItem.vue'
-import TodoForm from '../TodoForm/TodoForm.vue'
+import TodoContainer from '../TodoContainer/TodoContainer.vue'
 
 export default {
   name: 'App',
   components: {
-    TodoItem,
-    TodoForm,
+    TodoContainer
   },
-
-  data() {
-    return {
-      todoItems: [
-        { id: 1, text: "i need to do this" },
-        { id: 2, text: "and this" },
-        { id: 3, text: "and also this" },
-        { id: 4, text: "unfortunately also this" },
-      ]
-    }
-  }
 }
 </script>
 
