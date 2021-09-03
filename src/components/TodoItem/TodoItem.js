ee325475ba944c24254c992479f8a50b9b75f425
Igor Melo <igoracmelo@protonmail.com>
2021-09-02 22:49:03 -0300

    Fixed ReferenceError on checking the todo checkbox

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,28 +1,3 @@
-const onCheck = (e) => {
-  let currentTodo = e.target.parentNode
-  if (this.done)
-    currentTodo.classList.add("done")
-  else
-    currentTodo.classList.remove("done")
-}
-
-const onDelete = (e) => {
-  let currentTodo = e.target.parentNode
-  currentTodo.classList.add("deleted")
-  setTimeout(() => currentTodo.remove(), 500)
-}
-
-const onFocus = (e) => {
-  let currentTodo = e.target.parentNode
-  currentTodo.classList.add("hover")
-
-}
-
-const onBlur = (e) => {
-  let currentTodo = e.target.parentNode
-  currentTodo.classList.remove("hover")
-}
-
 export default {
   props: {
     todo: String,
@@ -35,9 +10,28 @@ export default {
   },
 
   methods: {
-    onCheck,
-    onDelete,
-    onFocus,
-    onBlur
+    onCheck(e) {
+      let currentTodo = e.target.parentNode
+      if (this.done)
+        currentTodo.classList.add("done")
+      else
+        currentTodo.classList.remove("done")
+    },
+    
+    onDelete(e) {
+      let currentTodo = e.target.parentNode
+      currentTodo.classList.add("deleted")
+      setTimeout(() => currentTodo.remove(), 500)
+    },
+    
+    onFocus(e) {
+      let currentTodo = e.target.parentNode
+      currentTodo.classList.add("hover")
+    },
+    
+    onBlur(e) {
+      let currentTodo = e.target.parentNode
+      currentTodo.classList.remove("hover")
+    }
   }
 }
