d27fb600d9707753836b3937e7b92e97d40b09d2
Igor Melo <igoracmelo@protonmail.com>
2021-09-03 02:08:53 -0300

    Adiós TodoContainer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,36 @@
-import TodoContainer from '../TodoContainer/TodoContainer.vue'
+import TodoForm from '../TodoForm/TodoForm.vue'
+import List from '../List/List.vue'
 
 export default {
   name: 'App',
   components: {
-    TodoContainer
+    TodoForm,
+    List,
+  },
+
+  data() {
+    return {
+      items: [
+        { id: 1, text: "i need to do this", done: false },
+        { id: 2, text: "and this", done: false },
+        { id: 10, text: "this is DONE", done: true },
+      ]
+    }
+  },
+
+  computed: {
+    todoItems() {
+      return this.items.filter((i) => !i.done)
+    },
+
+    doneItems() {
+      return this.items.filter((i) => i.done)
+    }
+  },
+
+  methods: {
+    addTodo(todo) {
+      this.items.push(todo)
+    },
   },
 }
