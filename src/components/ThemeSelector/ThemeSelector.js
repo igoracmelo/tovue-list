d11ce445ca755aab6fdb756a7f43d0ae2e8a6281
Igor Melo <igoracmelo@protonmail.com>
2021-10-24 12:09:21 -0300

    feat: save and load theme in/from localStorage
    Theme is automatically saved in localStorage, and when page is refreshed it is loaded from localStorage

--- src/components/ThemeSelector/ThemeSelector.js
+++ src/components/ThemeSelector/ThemeSelector.js
@@ -9,3 +9,18 @@ export default {
 		}
 	},
 
+	mounted() {
+		this.app = document.querySelector('#app')
+		let theme = localStorage.getItem('theme')
+		if (theme)
+			this.currentTheme = theme
+		this.app.className = this.currentTheme
+	},
+
+	watch: {
+		currentTheme(newTheme) {
+			localStorage.setItem('theme', newTheme)
+			this.app.className = newTheme
+		}
+	}
+}
