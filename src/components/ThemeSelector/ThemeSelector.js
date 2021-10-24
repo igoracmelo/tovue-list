993add6dbd3bebeb009c0baea7b5cf110166509a
Igor Melo <igoracmelo@protonmail.com>
2021-10-24 12:06:23 -0300

    feat: add theme selector

---
+++ src/components/ThemeSelector/ThemeSelector.js
@@ -0,0 +1,11 @@
+export default {
+	data() {
+		return {
+			themes: [
+				"dark",
+				"light"
+			],
+			currentTheme: 'light'
+		}
+	},
+
---
+++ src/components/ThemeSelector/ThemeSelector.vue
@@ -0,0 +1,14 @@
+<template>
+	<select v-model="currentTheme">
+		<option 
+			v-for="theme in themes" 
+			:key="theme" 
+			:value="theme"
+		>
+			{{ theme }}
+		</option>
+	</select>
+</template>
+
+
+<script src="./ThemeSelector.js"></script>
